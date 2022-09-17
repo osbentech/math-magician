cd68a6872dc6078089c1260d9e5200091073422e
Benjamin Ejike <benmma13@gmail.com>
2022-09-17 21:01:09 +0400

    Fix linters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,46 @@
-import React from "react";
+import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
-      return (
-        <div className="caclContainer">
-          <div className="theCalculator">
-            <input className="editable" value={0} />
-            <div className="inputValues">
-              <div className="row1">
-                <button type="button" className="AC"> AC</button>
-                <button type="button" className="plusNminus">+/-</button>
-                <button type="button" className="percentage">%</button>
-                <button type="button" className="division  arithmetic">/</button>
-              </div>
-              <div className="row2">
-                <button type="button" className="seven">7</button>
-                <button type="button" className="eight">8</button>
-                <button type="button" className="nine">9</button>
-                <button type="button" className="multiply  arithmetic">X</button>
-              </div>
-              <div className="row3">
-                <button type="button" className="four">4</button>
-                <button type="button" className="five">5</button>
-                <button type="button" className="six">6</button>
-                <button type="button" className="minus  arithmetic">-</button>
-              </div>
-              <div className="row4">
-                <button type="button" className="one">1</button>
-                <button type="button" className="two">2</button>
-                <button type="button" className="three">3</button>
-                <button type="button" className="plus  arithmetic">+</button>
-              </div>
-              <div className="row5">
-                <button type="button" className="zero">0</button>
-                <button type="button" className="dot">.</button>
-                <button type="button" className="equals  arithmetic">=</button>
-              </div>
+    return (
+      <div className="caclContainer">
+        <div className="theCalculator">
+          <input className="editable" value={0} />
+          <div className="inputValues">
+            <div className="row1">
+              <button type="button" className="AC"> AC</button>
+              <button type="button" className="plusNminus">+/-</button>
+              <button type="button" className="percentage">%</button>
+              <button type="button" className="division  arithmetic">/</button>
+            </div>
+            <div className="row2">
+              <button type="button" className="seven">7</button>
+              <button type="button" className="eight">8</button>
+              <button type="button" className="nine">9</button>
+              <button type="button" className="multiply  arithmetic">X</button>
+            </div>
+            <div className="row3">
+              <button type="button" className="four">4</button>
+              <button type="button" className="five">5</button>
+              <button type="button" className="six">6</button>
+              <button type="button" className="minus  arithmetic">-</button>
+            </div>
+            <div className="row4">
+              <button type="button" className="one">1</button>
+              <button type="button" className="two">2</button>
+              <button type="button" className="three">3</button>
+              <button type="button" className="plus  arithmetic">+</button>
+            </div>
+            <div className="row5">
+              <button type="button" className="zero">0</button>
+              <button type="button" className="dot">.</button>
+              <button type="button" className="equals  arithmetic">=</button>
             </div>
           </div>
         </div>
-      );
+      </div>
+    );
   }
-  }
+}
 export default Calculator;
